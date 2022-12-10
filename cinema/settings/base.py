029213398834ae71bef763b96e6f07d642444765
juanchotello98 <tello.juan@correounivalle.edu.co>
2022-12-10 05:41:48 -0500

    swagger

--- cinema/settings/base.py
+++ cinema/settings/base.py
@@ -116,6 +116,8 @@ CORS_ORIGIN_WHITELIST = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 #STATIC_URL = 'static/'
 
 # Default primary key field type
