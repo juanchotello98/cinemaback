1aeeb89871822a32665c4723afa9d34231e73ae6
juanchotello98 <tello.juan@correounivalle.edu.co>
2022-12-26 02:42:08 -0500

    cors config

--- cinema/settings/base.py
+++ cinema/settings/base.py
@@ -111,10 +111,15 @@ USE_TZ = True
 
 AUTH_USER_MODEL = 'users.User'
 
+CORS_ORIGIN_ALLOW_ALL = True   
+CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8000",
-    "http://localhost:3000"
+    "http://localhost:4200",
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:4200', 
 ]
+
 
 
 """
